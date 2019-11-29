7100dea26fddbfa0e06a3c96ba50cfccb332fc66
taiki011609 <s.taiki011609@gmail.com>
2019-11-29 17:45:30 +0900

    Create Random-intro

--- js_superintro/html2.js
+++ js_superintro/html2.js
@@ -1,7 +1,9 @@
 //html内で <script src="script.js"></script> を
 // <body>内に入力して使用
 
+//floor: randomの数を整数に整える
 let number = Math.floor(Math.random()*6);
+//parseInt: 入力された文字列を整数表示にする
 let answer = parseInt(window.prompt('数当てゲーム。０～５の数字を入力してね'));
 let message;
 if(answer === number){
