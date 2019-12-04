ddf7bc7256b2beb2352190fc648936e09f946005
taiki011609 <s.taiki011609@gmail.com>
2019-12-04 12:47:22 +0900

    Create im/export-imtro

---
+++ progate/js/prog5.js/script.js
@@ -0,0 +1,8 @@
+// importすることで、他のファイルで定義したモジュールを利用できる
+// ここでは、dog1とdog2 を ./data/dogData からインポートしている
+import { dog1, dog2 } from "./data/dogData";
+
+console.log("---------");
+dog1.info();
+console.log("---------");
+dog2.info();
