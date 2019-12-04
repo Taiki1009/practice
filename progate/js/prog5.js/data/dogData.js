2e4d2569119ecc9c2d4e88c2d2cbaeaa7db57109
taiki011609 <s.taiki011609@gmail.com>
2019-12-04 12:49:09 +0900

    Create im/export-intro

---
+++ progate/js/prog5.js/data/dogData.js
@@ -0,0 +1,9 @@
+// "./dog"(相対パス)を書き換えてください。
+import Dog from "../class/dog";
+
+
+// ユーザー情報をここで管理する
+const dog1 = new Dog("レオ", 4, "チワワ");
+const dog2 = new Dog("ベン", 2, "プードル");
+
+export { dog1, dog2 };
