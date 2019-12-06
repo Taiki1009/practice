dbde5eaca87c1d24295488850840a95f9391c6d3
taiki011609 <s.taiki011609@gmail.com>
2019-12-06 16:09:17 +0900

    Create switch/ArrayinArray-intro

---
+++ progate/js/prog1-2.js
@@ -0,0 +1,42 @@
+const rank = 5;
+
+switch (rank) {
+  case 1:
+    console.log("金メダルです！");
+    break;
+  case 2:
+    console.log("銀メダルです！");
+    break;
+  case 3:
+    console.log("銅メダルです！");
+    break;
+  // defaultの処理を追加してください
+  default:
+  console.log("メダルはありません");
+  break;
+  
+}
+
+
+
+
+const cafe = {
+  name: "Progateカフェ",
+  businessHours: { 
+    opening: "10:00(AM)",
+    closing: "8:00(PM)"
+  },
+  // menusプロパティに配列を代入してください
+  menus: ["コーヒー","紅茶","チョコレートケーキ"]
+  
+};
+
+console.log(`店名: ${cafe.name}`);
+console.log(`営業時間:${cafe.businessHours.opening}から${cafe.businessHours.closing}`);
+console.log(`----------------------------`);
+console.log("おすすめメニューはこちら");
+
+// for文を用いて配列menusの中身を表示させてください
+for(let i=0; i < cafe.menus.length; i++){
+  console.log(cafe.menus[i]);
+}
