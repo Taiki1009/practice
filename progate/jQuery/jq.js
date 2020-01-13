df401dca2a391787bc77e203d3d71ce2aca94e3e
taiki011609 <s.taiki011609@gmail.com>
2020-01-13 09:51:58 +0900

    Create jquery-intro

---
+++ progate/jQuery/jq.js
@@ -0,0 +1,46 @@
+基本構文
+
+$('セレクタ').fadeOut(mili-second);
+
+    // セレクタ　＝　[id: #name, class: .name];
+
+
+イベント
+
+$('イベントセレクタ').イベント名(function(){
+    $('セレクタ').Animetion();
+    $('セレクタ').css('プロパティ','値');
+    $('セレクタ').html('タグ');
+    $('this').css('','')     //親要素のイベントセレクタがthisに代入
+
+});
+
+
+$(function() {
+    $('#find-method').click(function() {
+      // findメソッドで、「#wrapper」内にあるすべての「a」要素を取得し、
+      $('#wrapper').find('a').css('color','red');
+    });
+    
+    $('#children-method').click(function() {
+      // childrenメソッドで、「#wrapper」の一階層下にある「a」要素を取得し、
+      $('#wrapper').children('a').fadeOut();
+    });
+
+});
+
+// hover: hover時と解除時の２つの関数をかく
+$('#language-wrapper').hover(
+    function(){
+      $('.language-text').fadeIn();
+    },
+    function(){
+      $('.language-text').fadeOut();
+
+    }
+);
+
+
+
+//  jQueryの読み込み: <head>内で読み込み、<body>の最下部でstylesheetを読みこむ
+https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
