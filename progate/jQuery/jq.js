34b5a2309cb7bcf928c8d3eb108b64847b30f926
taiki011609 <s.taiki011609@gmail.com>
2020-01-20 17:31:51 +0900

    Create jq-intro

--- progate/jQuery/jq.js
+++ progate/jQuery/jq.js
@@ -1,36 +1,30 @@
-基本構文
+    基本構文
 
 $('セレクタ').fadeOut(mili-second);
+  // セレクタの書き方　＝　[id: #name, class: .name];
 
-    // セレクタ　＝　[id: #name, class: .name];
 
 
-イベント
+
+    イベント
 
 $('イベントセレクタ').イベント名(function(){
     $('セレクタ').Animetion();
-    $('セレクタ').css('プロパティ','値');
-    $('セレクタ').html('タグ');
-    $('this').css('','')     //親要素のイベントセレクタがthisに代入
+    $('セレクタ').html('タグ');        //セレクタのタグを変更
+    $(this).css('プロパティ','値');    //親要素のイベントセレクタがthisに代入
 
 });
 
-
+// findメソッドで、セレクタ内にあるすべての「a」要素を取得
+// childrenメソッドなら、直下の子要素のみ取得できる
 $(function() {
-    $('#find-method').click(function() {
-      // findメソッドで、「#wrapper」内にあるすべての「a」要素を取得し、
-      $('#wrapper').find('a').css('color','red');
+    $('イベントセレクタ').click(function() {
+      $('セレクタ').find('a').css('color','red');
     });
-    
-    $('#children-method').click(function() {
-      // childrenメソッドで、「#wrapper」の一階層下にある「a」要素を取得し、
-      $('#wrapper').children('a').fadeOut();
-    });
-
 });
 
 // hover: hover時と解除時の２つの関数をかく
-$('#language-wrapper').hover(
+$('イベントセレクタ').hover(
     function(){
       $('.language-text').fadeIn();
     },
