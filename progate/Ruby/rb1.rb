5aa82e72396b734ca71454bcbbcc9c10a8a0b34f
taiki011609 <s.taiki011609@gmail.com>
2020-01-26 19:50:28 +0900

    Create If&Array

---
+++ progate/Ruby/rb1.rb
@@ -0,0 +1,37 @@
+# lesson1
+number = 60
+
+if number % 3 == 0 && number % 5 == 0
+    puts "15の倍数です"
+    
+    elsif number % 5 == 0
+    puts "5の倍数です"
+    
+    elsif number % 3 == 0
+    puts "3の倍数です"
+    
+    else
+    puts "3の倍数でも5の倍数でもありません"
+end
+
+
+# lesson2
+characters = [
+    {name: "にんじゃわんこ", age: 14},
+    {name: "ひつじ仙人"},
+    {name: "ベイビーわんこ", age: 5},
+    {name: "とりずきん"}
+]
+
+characters.each do |character|
+    puts "--------------------"
+    puts "名前は#{character[:name]}です"
+    
+    # キー:ageの値に応じて年齢の情報を出力してください
+    if character[:age]
+        puts "年齢は#{character[:age]}歳です"
+    else
+        puts "年齢は秘密です"
+    end
+    
+end
