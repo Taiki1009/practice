a6d38be73c3de2e088b4abc68336f807b7bb4c07
taiki011609 <s.taiki011609@gmail.com>
2020-01-26 20:40:56 +0900

    Create If&Array&return

--- progate/Ruby/rb1.rb
+++ progate/Ruby/rb1.rb
@@ -35,3 +35,21 @@ characters.each do |character|
     end
     
 end
+
+
+# lesson3
+def price_with_shipping(price)
+	if price >= 50000
+	    return price
+	end
+	return price + 1000
+end
+def buy(item:, price:, count:)
+    total_price = price * count
+    puts "-----------------------------------"
+	puts "#{item}を#{count}台のお買い上げです"
+    puts "商品の合計金額は#{total_price}円です"
+    puts "お支払い金額は、送料込みで#{price_with_shipping(total_price)}円です"
+end
+
+buy(price: 15000,item: "テレビ",count: 3)
