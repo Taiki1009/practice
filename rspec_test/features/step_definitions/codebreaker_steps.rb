d97eff4b8c5f460640144d53df1cd7a5b97769b2
taiki011609 <s.taiki011609@gmail.com>
2021-01-24 10:04:02 +0900

    RSpecbook 7/25

--- rspec_test/features/step_definitions/codebreaker_steps.rb
+++ rspec_test/features/step_definitions/codebreaker_steps.rb
@@ -2,11 +2,9 @@ class Output
   def messages
     @messages ||= []
   end
-
   def puts(message)
     messages << message
   end
-
   def output
     @output ||= Output.new
   end
@@ -18,9 +16,15 @@ end
 
 When /^I start a new game$/ do
   game = Codebreaker::Game.new(output)
-  game.start
+  game.start('1234')
 end
 
 Then /^ Ishould see "([^"]*)"$/ do |message|
   expect(output.messages).to include(message)
+end
+
+
+Given /^the secret code is "([^"]*)"$/ do |secret|
+  game = Codebreaker::Game.new(output)
+  game.start(secret)
 end
--- rspec_test/spec/codebreaker/game_spec.rb
+++ rspec_test/spec/codebreaker/game_spec.rb
@@ -2,21 +2,40 @@ require 'spec_helper'
 
 module Codebreaker
   RSpec.describe Game do
+    # モック・テストダブルの作成
+    let(:output)  { double('output').as_null_object }
+    let(:game) { Game.new(output) }
+    
     describe "#start" do
-      # モック・テストダブルの作成
-      let(:output)  { double('output').as_null_object }
-      let(:game) { Game.new(output) }
-
       it "sends a welcome message" do
         expect(output).to receive(:puts).with('Welcome to Codebreaker')
-
-        game.start
+        game.start('1234')
       end
 
       it "prompts for the first guess" do
         expect(output).to receive(:puts).with('Enter guess:')
+        game.start('1234')
+      end
+    end
 
-        game.start
+    describe "#guess" do
+      context "with no match" do
+        it "send a mark with ''" do
+          # 正解のcodeを覚えさせる
+          game.start('1234')
+          # 正解のmarkを覚えさせる
+          expect(output).to receive(:puts).with('')
+          # 予想のguessを判定させてmarkと一致するかテスト
+          game.guess('5555')
+        end
+      end
+
+      context "with 1 number match" do
+        it "sends a mark with '-'" do
+          game.start('1234')
+          expect(output).to receive(:puts).with('-')
+          game.guess('2555')
+        end
       end
     end
   end
